bc12b597ff6bb5b0cd0c3b4ef1fe55253f23cc18
Stefan Wanitzek <stefan@dreambyte.eu>
2015-08-19 21:37:33 +0200

    Send google cloud-message

---
+++ source/config.js
@@ -0,0 +1,9 @@
+var config = {};
+
+config.gcm = {};
+
+config.gcm.apikey = '<INSERT API-KEY HERE>';
+
+config.port = 3333;
+
+module.exports = config;
--- source/server.js
+++ source/server.js
@@ -1,8 +1,10 @@
 // a simple variable will store the GCM-ID for now, will be replaced with a database in the future
-var tempGcmId = null
+var tempGcmId = null;
 
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var gcm = require('node-gcm');
+var config = require('./config');
 
 // print the current GCM-Id to the browser
 app.get('/', function (req, res) {
@@ -23,5 +25,44 @@ app.get('/setGcmId', function (req, res) {
   res.send( 'Id set: ' + id );
 });
 
+
+app.get('/ring', function (req, res) {
+  if (tempGcmId == null)
+  {
+    res.send('Id not set!');
+  } else {
+    var message = new gcm.Message();
+
+    // ... or some given values
+    var message = new gcm.Message({
+		    contentAvailable: true,
+		    delayWhileIdle: false,
+    });
+
+    message.addData('key1','message1');
+
+    var sender = new gcm.Sender(config.gcm.apikey);
+
+    console.log(config.gcm.apikey);
+	 
+    // Add the registration IDs of the devices you want to send to 
+    var registrationIds = [];
+    registrationIds.push(tempGcmId);
+
+    // Send the message 
+    // ... trying only once 
+    sender.sendNoRetry(message, registrationIds, function(err, result) {
+	      if (err) 
+	      {
+	      	console.error(err);
+	      	res.send(err);
+	      } else {
+			console.log(result);
+			res.send(result);
+	      }    
+      });
+	}
+});
+
 // TODO => create a config file to configure server-port etc.
-app.listen(3000)
+app.listen(config.port)
